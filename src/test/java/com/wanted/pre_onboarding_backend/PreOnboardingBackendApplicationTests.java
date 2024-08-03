294a98fb68294493d451d09d028d971f6bf134fa
Jinhui <jda0526@naver.com>
2024-08-03 19:42:44 +0900

    초기 세팅 파일

---
+++ src/test/java/com/wanted/pre_onboarding_backend/PreOnboardingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanted.pre_onboarding_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreOnboardingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
